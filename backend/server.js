351339ed170647dddb7566dc70ab2eddf3807054
jainnishant1 <njnishantjain99@gmail.com>
2022-01-25 22:03:41 +0530

    Setting backend and cors

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import data from './data.js';
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors())
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,5 @@
-// import React from 'react';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import Product from './components/Product';
-// import data from './data';
-// function App() {
-//   return (
-//     <div className="grid-container">
-//       <header className="row">
-//         <div>
-//           <a className="brand" href="/">
-//             amazona
-//           </a>
-//         </div>
-//         <div>
-//           <a href="/cart">Cart</a>
-//           <a href="/signin">Sign In</a>
-//         </div>
-//       </header>
-//       <main>
-//         <div>
-//           <div className="row center">
-//             {data.products.map((product) => (
-//               <Product key={product._id} product={product}></Product>
-//             ))}
-//           </div>
-//         </div>
-//       </main>
-//       <footer className="row center">All right reserved</footer>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
