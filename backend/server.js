3aaaad377d7d4becf687067c9f982724cb8ff51d
M0706 <garg.6@iitj.ac.in>
2022-01-27 13:29:42 +0530

    sample seed mongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,37 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors'
+import mongoose from 'mongoose'
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+import dotenv from "dotenv"
 
 const app = express();
-
+dotenv.config();
 app.use(cors())
 
-app.get('/api/products/:id',(req,res)=>{
-  const product = data.products.find(x=> x._id===req.params.id);
-  if(product){
-    res.send(product);
-  }
-  else{
-    res.status(404).send({message: 'Product not found'})
-  }
-})
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use('/api/users',userRouter);
+app.use('/api/products',productRouter);
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
+
+app.use((err,req,res,next)=>{
+  res.status(500).send({message: err.message})
+})
+
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server at http://localhost:${port}`);
-});
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.b2qfc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(result=>{
+  app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+  });
+}).catch(err=>{
+  console.log("error while connecting===>",err)
+})
